1a8afa43e474cddfa9171c1f8b73635fe3aed4d7
gnanapazham.navachandran <gnanapazham.navachandran@tnqtech.com>
2026-09-11 12:21:44 +0530

    Route handler

--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
 const express = require("express");
 
 const app = express();
-app.use("/", (req, res) => {
-    res.send("New Web Page");
+
+app.use("/user", (req, res) => {
+    res.send('scrum');
+});
+
+app.get("/user", (req, res) => {
+    res.send({ name: 'gnanam', role: 'developer' });
 });
 
 app.use("/test", (req, res) => {
     res.send("New test Page");
 });
 
+app.use("/hello/2", (req, res) => {
+    res.send("New hello Page");
+});
+
 app.use("/hello", (req, res) => {
     res.send("New hello Page");
+});
+
+app.use("/", (req, res) => {
+    res.send("New Web Page");
 });
 
 app.listen(7777, () => {
